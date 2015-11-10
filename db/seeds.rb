5b433f4647fc3f5e13d332516b6aba6593033d5f
Danya D. Smith <danyadsmith@gmail.com>
2015-11-09 22:51:16 -0500

    Enhance seed.db File
    Modify the seed.db file such that it generates:
    
      7 Random Users
      3 Role-Based Users
     20 Public Wikis
    100 Public Wiki Articles
      5 Private Wikis
    100 Private Wiki Articles

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,26 +1,89 @@
 # ##############################################################
-# Create Users (Standard)
+# Create Users (10)
 # ##############################################################
 
 7.times do
   user = User.create!(
-    name: Faker::Name.name,
     email: Faker::Internet.email,
-    password: Faker::Internet.password(10)
+    password: Faker::Internet.password(10),
     role: 0
   )
 end
 
+standard_user = User.create!(
+  email: "standard@blocipedia.com",
+  password: "blocipedia",
+  role: 0
+)
+
+premium_user = User.create!(
+  email: "premium@blocipedia.com",
+  password: "blocipedia",
+  role: 1
+)
+
+admin_user = User.create!(
+  email: "admin@blocipedia.com",
+  password: "blocipedia",
+  role: 2
+)
+
 users = User.all
 
 # ##############################################################
-# Create Public Wikis
+# Create Public Wikis (20)
 # ##############################################################
 
 20.times do
   wiki = Wiki.create!(
     title: Faker::Lorem.sentence,
-    description: Faker::Lorem.sentence(100)
-    user: users.
+    description: Faker::Lorem.sentence(3),
+    user: users.sample,
+    private: false
   )
+  wiki.update_attribute(:created_at, rand(10.minutes .. 1.year).ago)
+end
+
+public_wikis = Wiki.where(private: false)
+
+# ##############################################################
+# Create Public Wiki Articles (100)
+# ##############################################################
+
+100.times do 
+  article = Article.create!(
+    title: Faker::Lorem.sentence,
+    body: Faker::Lorem.paragraph(100),
+    wiki: public_wikis.sample
+  )
+  article.update_attribute(:created_at, rand(10.minutes .. 1.year).ago)
+end
+
+# ##############################################################
+# Create Private Wikis (5)
+# ##############################################################
+
+5.times do
+  wiki = Wiki.create!(
+    title: Faker::Lorem.sentence,
+    description: Faker::Lorem.sentence(3),
+    user: users[rand(8..9)],
+    private: true
+  )
+  wiki.update_attribute(:created_at, rand(10.minutes .. 1.year).ago)
+end
+
+private_wikis = Wiki.where(private: true)
+
+# ##############################################################
+# Create Private Wiki Articles (100)
+# ##############################################################
+
+100.times do 
+  article = Article.create!(
+    title: Faker::Lorem.sentence,
+    body: Faker::Lorem.paragraph(100),
+    wiki: private_wikis.sample
+  )
+  article.update_attribute(:created_at, rand(10.minutes .. 1.year).ago)
 end
